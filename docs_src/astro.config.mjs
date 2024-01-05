76a1fa92328534fc17a50371a65678086e225af8
Neil Musgrove <neil.musgrove@panintelligence.com>
2024-01-05 00:36:45 +0000

    Fix base setting

--- docs_src/astro.config.mjs
+++ docs_src/astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://panintelligence.github.io',
-	base: '/docker-deployments/docs',
+	base: '/docker-deployments',
 	integrations: [
 		starlight({
 			title: 'Panintelligence Container Deployments',
